76c335575fcd5121811f9446f07c014fbee33439
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-05-05 12:53:02 +0800

    update setup

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
+
+def get_version():
+    """Get version number from __init__.py"""
+    version_file = Path(__file__).resolve().parent / "cblaster" / "__init__.py"
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read_text(), re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Failed to find version string")
+
+
 setup(
     name="fungphy",
     author="Cameron Gilchrist",
@@ -17,4 +29,5 @@ setup(
     ],
     install_requires=["ete3", "flask", "flask-sqlalchemy", "flask-admin"],
     python_requires=">=3.6",
+    entry_points={"console_scripts": ["fungphy=fungphy.main:main"]},
 )
