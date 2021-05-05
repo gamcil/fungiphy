0fc41ad9c3d0b6d9741ae402c83f26f3ee69c2b5
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-05-05 12:53:12 +0800

    add alembic

---
+++ alembic/versions/079dae11cc2f_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: 079dae11cc2f
+Revises: 
+Create Date: 2020-04-05 13:46:39.061350
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '079dae11cc2f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
